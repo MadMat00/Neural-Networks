850ff2576ee89bbf1b6758724aaafe19cb48c3d5
Mattia Rossini <m.rossini@itsrizzoli.it>
2023-07-22 17:35:18 +0200

    Basic structure of a single neuron

---
+++ neuron.py
@@ -0,0 +1,11 @@
+#inputs given to the neuron
+inputs = [1.2, 5.1, 2.1]
+
+#weights of the inputs
+weights = [3.1, 2.1, 8.7]
+
+#bias of the neuron
+bias = 3
+
+#output of the neuron
+output = inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + bias
