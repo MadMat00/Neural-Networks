942e074a01bb22cb942434d7d9d08ad9f9d57c10
Mattia Rossini <m.rossini@itsrizzoli.it>
2023-07-22 17:53:35 +0200

    Code optimization

--- neuron.py
+++ neuron.py
@@ -1,11 +1,13 @@
 #inputs given to the neuron
 inputs = [1.2, 5.1, 2.1]
-
-#weights of the inputs
 weights = [3.1, 2.1, 8.7]
 
-#bias of the neuron
 bias = 3
+output = 0
 
-#output of the neuron
-output = inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + bias
+for i in range(len(inputs)):
+    output += inputs[i] * weights[i]
+
+output += bias
+
+print(output)
